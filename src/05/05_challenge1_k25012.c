80798618f82cfac74965df486abb4400df73e0fb
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-05-15 13:57:57 +0900

    🩹 challengeを配列使わないようにした
    配列使っちゃいけないなんて聞いてないんだが？はやく授業で配列くらいやって欲しい；；；

--- src/05/05_challenge1_k25012.c
+++ src/05/05_challenge1_k25012.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 
 int main() {
-    // 日=0, 月=1, 火=2, 水=3, 木=4, 金=5, 土=6
     int dayOfWeek;
-    // 09:00~12:00=0, 13:00~16:00=1
     int time;
-    int isOpen[7][2] = {
-        {0, 0}, {1, 1}, {1, 1}, {1, 0}, {1, 1}, {1, 1}, {0, 1}
-    };
-    
+    int isOpen = 0;
+
     printf("曜日? ");
     scanf("%d", &dayOfWeek);
     printf("時間帯? ");
     scanf("%d", &time);
-    
-    if(isOpen[dayOfWeek][time]) {
-        printf("診察を行っています\n");
+
+    if (time == 0) {
+        if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 3 || dayOfWeek == 4 || dayOfWeek == 5) {
+            isOpen = 0;
+        }
     }
-    else {
+
+    if (time == 1) {
+        if (dayOfWeek == 1 || dayOfWeek == 2 || dayOfWeek == 4 || dayOfWeek == 5 || dayOfWeek == 6) {
+            isOpen = 1;
+        }
+    }
+    
+    if (isOpen == 1) {
+        printf("診察を行っています\n");
+    } else {
         printf("診察は行っていません\n");
     }
+    // 日=0, 月=1, 火=2, 水=3, 木=4, 金=5, 土=6
+    // int dayOfWeek;
+    // // 09:00~12:00=0, 13:00~16:00=1
+    // int time;
+    // int isOpen[7][2] = {
+    //     {0, 0}, {1, 1}, {1, 1}, {1, 0}, {1, 1}, {1, 1}, {0, 1}
+    // };
     
-    return 0;
+    // printf("曜日? ");
+    // scanf("%d", &dayOfWeek);
+    // printf("時間帯? ");
+    // scanf("%d", &time);
+    
+    // printf(isOpen[dayOfWeek][time] ? "診察を行っています" : "診察は行っていません");
+    // printf("\n");
+    
+    // return 0;
 }
