8d3501c41d7a753e654fdcb12c05f239d21e8562
suzuuuuu09 <140299273+suzuuuuu09@users.noreply.github.com>
2025-05-29 12:10:22 +0900

    ✨ 09

---
+++ src/09/09_challenge1_k25012.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+  char ch;
+  int isMinus = 0;
+  int answer = 0;
+  int inputNumber = 0;
+
+  printf("equation? ");
+  while ((ch = getchar()) != '=') {
+    switch (ch) {
+      case '-':
+        isMinus = 1;
+        break;
+      case '+':
+        isMinus = 0;
+        break;
+      case ' ':
+        if (isMinus) {
+          answer -= inputNumber;
+        } else {
+          answer += inputNumber;
+        }
+        inputNumber = 0;
+        break;
+      default:
+        // 数字
+        if (ch >= '0' && ch <= '9') {
+          inputNumber = ch - '0';
+        }
+        break;
+    }
+  }
+  
+  if (isMinus) {
+    answer -= inputNumber;
+  } else {
+    answer += inputNumber;
+  }
+  
+  printf("answer: %d\n", answer);
+  return 0;
+}
